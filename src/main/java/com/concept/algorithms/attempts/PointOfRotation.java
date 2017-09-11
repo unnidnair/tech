60c17b94a70aae825c022cebf898c6439d1832c2
Smitha Ammassamveettil <vzw.smitha@outlook.com>
2017-09-10 17:36:42 -0700

    no message

---
+++ src/main/java/com/concept/algorithms/attempts/PointOfRotation.java
@@ -0,0 +1,32 @@
+package com.concept.algorithms.attempts;
+
+//find-the-point-of-rotation-in-sorted-array
+//http://techieme.in/find-the-point-of-rotation-in-sorted-array/
+public class PointOfRotation {
+
+	public static void main(String[] args) {
+		int[] a = {10,11,12,13,14,6,8,9};
+		PointOfRotation pOfRotation = new PointOfRotation();
+		pOfRotation.findPointOfRotation(a);
+	}
+	
+	//10,11,12,13,14,6,8,9
+	//10,11,12
+	//10,11,12,13,14,6
+	//14,6,8,9
+
+	private void findPointOfRotation(int[] a) {
+		
+		int pointOfRotation = -1;
+		if (null!=a || 0!=a.length) {
+			int start=0, end=a.length-1, mid;
+			mid=start+(end-start)/2;
+///FILL CODE
+
+			
+			
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/concept/features/concurrent/ExchangerExample.java
@@ -0,0 +1,43 @@
+package com.concept.features.concurrent;
+
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.TimeUnit;
+
+public class ExchangerExample {
+
+	Exchanger<String> exchanger = new Exchanger<String>();
+
+	private void start() {
+		new Thread(() -> {
+			try {
+				for (int index = 0; index < 10; index++) {
+					exchanger.exchange("Ready Queue " + index);
+					TimeUnit.SECONDS.sleep(1);
+				}
+				exchanger.exchange("EXIT");
+			} catch (Exception e) {
+			}
+		}).start();
+
+		new Thread(r, "Consumer").start();
+		new Thread(r, "Consumer1").start();
+	}
+
+	Runnable r = () -> {
+		try {
+			while (true) {
+				String queue = exchanger.exchange("received");
+				if (queue.equals("EXIT")) {
+					break;
+				}
+				System.out.println(Thread.currentThread().getName() + " now has " + queue);
+			}
+		} catch (InterruptedException e) {
+		}
+	};
+
+	public static void main(String[] args) {
+		new ExchangerExample().start();
+	}
+
+}
